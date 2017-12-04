4ab26165aa7d0958127c1a9e458db1acda297048
aosuna <osuna.9@gmail.com>
2017-12-03 19:15:25 -0800

    fixed scrolling function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
+// active scrolling
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top - 50
      }, 500);
 });
 
 // Change the active navbar item on scroll
-var sections = $('.section-item'),
+let sections = $('.section-item'),
     nav = $('.navbar'),
     nav_height = nav.outerHeight();
 
 $(window).on('scroll', function (){
-  var cur_pos = $(this).scrollTop();
+  let cur_pos = $(this).scrollTop();
   sections.each(function(){
-    var top = $(this).offset().top - nav_height,
+    let top = $(this).offset().top - nav_height,
         bottom = top + $(this).outerHeight();
 
     if (cur_pos >= top && cur_pos <= bottom){
